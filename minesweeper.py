964b0c49f2b9048d2538794eb53308bdb89a1e85
DarkKnight450 <64155209+DarkKnight450@users.noreply.github.com>
2022-01-31 10:36:23 +0530

    Initial Commit

---
+++ minesweeper.py
@@ -0,0 +1,244 @@
+import argparse
+import copy
+import curses
+import random
+import time
+from curses import wrapper
+
+parser = argparse.ArgumentParser(description="CLI Minesweeper written in Python3")
+parser.version = "1.0 [beta]"
+parser.add_argument("-v", "--version", action="version")
+parser.add_argument(
+    "-r", metavar="rows", type=int, help="Number of rows in the grid [Defaults to 10]"
+)
+parser.add_argument(
+    "-c",
+    metavar="colums",
+    type=int,
+    help="Number of columns in the grid [Defaults to 10]",
+)
+parser.add_argument(
+    "-m",
+    metavar="mines",
+    type=int,
+    help="Total number of mines [Defaults to 10]. If you set an abnormal value, the program will try to normalise the number of mines on its own.",
+)
+args = parser.parse_args()
+rows = args.r if args.r else 10
+cols = args.c if args.c else 10
+total_mines = args.m if args.m else 10
+## normalize number of mines
+total_mines = total_mines if total_mines <= rows * cols - 3 else rows * cols - 8
+
+
+def get_neighbor_coords(board_, cell_x, cell_y):
+    """
+    Takes  in x, y format
+    Returns in x, y format
+    """
+    rows_, cols_ = len(board_), len(board_[0])
+    coords = []
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if i == j == 0:
+                continue
+            if (0 <= cell_y + i < rows_) and (0 <= cell_x + j < cols_):
+                coords.append((cell_x + j, cell_y + i))
+    return coords
+
+
+def generate_board(
+    rows_, cols_, total_mines_, starting_cell_x_, starting_cell_y_
+) -> list:
+
+    ## initialize empty board
+    board_ = []
+    for y_ in range(rows_):
+        _ = []
+        for x_ in range(cols_):
+            _.append(" ")
+        board_.append(_)
+
+    available_cells_for_mines = []
+    for y_ in range(rows_):
+        for x_ in range(cols_):
+            available_cells_for_mines.append((x_, y_))
+
+    ## we want the starting cell to be always safe
+    available_cells_for_mines.remove((starting_cell_x_, starting_cell_y_))
+    for coord_ in get_neighbor_coords(board_, starting_cell_x_, starting_cell_y_):
+        available_cells_for_mines.remove(coord_)
+
+    ## place mines
+    for _ in range(total_mines_):
+        mine_coord = random.choice(available_cells_for_mines)
+        board_[mine_coord[1]][mine_coord[0]] = "*"
+        available_cells_for_mines.remove(mine_coord)
+    for y_ in range(rows_):
+        for x_ in range(cols_):
+            if board_[y_][x_] == "*":
+                continue
+            mine_count = 0
+            for coord_ in get_neighbor_coords(board_, x_, y_):
+                if board_[coord_[1]][coord_[0]] == "*":
+                    mine_count += 1
+            board_[y_][x_] = mine_count
+
+    return board_
+
+
+cells_to_be_revealed = []
+
+
+def floodfill(board_, x_, y_):
+    ## generic floodfill algoritm - recurisively nukes cells with neighbors having zero mines
+    for coord_ in get_neighbor_coords(board_, x_, y_):
+        if board_[coord_[1]][coord_[0]] == 0:
+            if coord_ not in cells_to_be_revealed:
+                cells_to_be_revealed.append(coord_)
+                floodfill(board_, coord_[0], coord_[1])
+        if coord_ not in cells_to_be_revealed:
+            cells_to_be_revealed.append(coord_)
+
+
+def main(stdscr):
+    curses.use_default_colors()
+    curses.init_pair(1, curses.COLOR_GREEN, -1)
+    GREEN = curses.color_pair(1)
+    curses.init_pair(2, curses.COLOR_RED, -1)
+    RED = curses.color_pair(2)
+    curses.init_pair(3, curses.COLOR_CYAN, -1)
+    CYAN = curses.color_pair(3)
+    curses.init_pair(4, curses.COLOR_YELLOW, -1)
+    YELLOW = curses.color_pair(4)
+    curses.init_pair(5, curses.COLOR_MAGENTA, -1)
+    MAGENTA = curses.color_pair(5)
+    curses.init_pair(6, curses.COLOR_BLUE, -1)
+    BLUE = curses.color_pair(6)
+    stdscr.addstr(0, 0, r"  ____    __  ____  ____   _  ______  ______  __  __  __  ______  ______  _____  ______  _____   ", BLUE)
+    stdscr.addstr(1, 0, r" |    \  /  ||    ||    \ | ||   ___||   ___||  \/  \|  ||   ___||   ___||     ||   ___||     |  ", BLUE)
+    stdscr.addstr(2, 0, r" |     \/   ||    ||     \| ||   ___| `-.`-. |     /\   ||   ___||   ___||    _||   ___||     \  ", CYAN)
+    stdscr.addstr(3, 0, r" |__/\__/|__||____||__/\____||______||______||____/  \__||______||______||___|  |______||__|\__\ ", CYAN)
+    stdscr.refresh()
+
+    def reveal(window, board_, j_, i_):
+        if str(board_[j_][i_]) == "*":
+            window.addch(j_, i_ * 4, "*", MAGENTA)
+        elif board_[j_][i_] == 0:
+            window.addch(j_, i_ * 4, " ")
+        elif board_[j_][i_] == 1:
+            window.addch(j_, i_ * 4, "1", CYAN)
+        elif board_[j_][i_] == 2:
+            window.addch(j_, i_ * 4, "2", GREEN)
+        elif board_[j_][i_] == 3:
+            window.addch(j_, i_ * 4, "3", YELLOW)
+        else:
+            window.addch(j_, i_ * 4, "4", RED)
+        window.refresh()
+
+    game_win = curses.newwin(2000, 2000, 6, 1)
+    game_win.addstr(0, cols*4+4, "     ┌─────┐            ")
+    game_win.addstr(1, cols*4+4, "     │  W  │                 UP")
+    game_win.addstr(2, cols*4+4, "┌────┴┬────┴┬─────┐     LEFT    RIGHT")
+    game_win.addstr(3, cols*4+4, "│  A  │  S  │  D  │         DOWN")
+    game_win.addstr(4, cols*4+4, "└─────┴─────┴─────┘")
+    game_win.addstr(5, cols*4+4, "")
+    game_win.addstr(6, cols*4+4, "┌─────────────────┐     ")
+    game_win.addstr(7, cols*4+4, "│      SPACE      │         MINE")
+    game_win.addstr(8, cols*4+4, "└─────────────────┘")
+
+    for y_ in range(rows):
+        delta_x = 0
+        for x_ in range(cols):
+            game_win.addch(y_, x_ + delta_x, "•", curses.A_BOLD)
+            game_win.refresh()
+            delta_x += 3
+
+    game_win.move(0, 0)  ## reset cursor position
+    DED = False
+    STARTED = False
+
+    while not DED:
+        curses.noecho()
+        curses.curs_set(1)
+        game_win.nodelay(True)
+        current_y, current_x = game_win.getyx()
+        key_pressed = game_win.getch()
+
+        try:
+            if key_pressed in (ord("Q"), ord("q")):
+                return
+            elif key_pressed in (ord("W"), ord("w")):
+                game_win.move(current_y - 1, current_x)
+            elif key_pressed in (ord("A"), ord("a")):
+                game_win.move(current_y, current_x - 4)
+            elif key_pressed in (ord("S"), ord("s")) and current_y < rows - 1:
+                game_win.move(current_y + 1, current_x)
+            elif key_pressed in (ord("D"), ord("d")) and current_x < cols * 4 - 4:
+                game_win.move(current_y, current_x + 4)
+
+            elif key_pressed == 32:  ## key pressed is a spacw
+                y_, x_ = game_win.getyx()
+                if not STARTED:
+                    board = generate_board(rows, cols, total_mines, x_ // 4, y_)
+                    board_cache = copy.deepcopy(board)
+                    STARTED = True
+                    START_TIME = time.perf_counter()
+
+                try:
+                    _cell_ = board[y_][x_ // 4]
+                except IndexError:
+                    pass
+
+                if _cell_ == 0:
+                    reveal(game_win, board, y_, x_ // 4)
+                    board_cache[y_][x_ // 4] = "#"
+                    floodfill(board, x_ // 4, y_)
+                    for i in range(cols):
+                        for j in range(rows):
+                            if (i, j) in cells_to_be_revealed:
+                                reveal(game_win, board, j, i)
+                                board_cache[j][i] = "#"
+
+                elif _cell_ == "*":  ## revealed cell is a mine
+                    for i in range(cols):
+                        for j in range(rows):
+                            reveal(game_win, board, j, i)
+                    game_win.nodelay(False)
+
+                    game_win.addstr(rows + 2, 0, "U is DED xD", RED)
+                    game_win.addstr(10, cols * 4 + 4, f"Time elapsed: {round(time.perf_counter()-START_TIME)}s", GREEN)
+                    game_win.addstr(rows + 3, 0, "Press any key to continue...", curses.A_BOLD)
+                    game_win.refresh()
+                    game_win.getch()
+                    DED = True
+
+                else:  ## revealed cell is a number
+                    reveal(game_win, board, y_, x_ // 4)
+                    board_cache[y_][x_ // 4] = "#"
+                game_win.move(y_, x_)
+
+                ## check if player has won already
+                cells_revealed = 0
+                for i in range(cols):
+                    for j in range(rows):
+                        if board_cache[j][i] == "#":
+                            cells_revealed += 1
+
+                if cells_revealed == rows * cols - total_mines:
+                    for i in range(cols):
+                        for j in range(rows):
+                            reveal(game_win, board, j, i)
+                    game_win.nodelay(False)
+                    game_win.addstr(rows + 2, 0, "You won!!", GREEN)
+                    game_win.addstr(10, cols * 4 + 4, f"Time elapsed: {round(time.perf_counter()-START_TIME)}s", GREEN)
+                    game_win.addstr(rows + 3, 0, "Press any key to continue...", curses.A_BOLD)
+                    game_win.refresh()
+                    game_win.getch()
+                    DED = True
+
+        except (curses.error, ValueError):
+            pass
+
+
+wrapper(main)
